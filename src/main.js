0e6cdc6c1eed59a0a79c91bf06e38c7cf0d3cd13
Mykhailo Ulanovych <97468374+UlanovychM@users.noreply.github.com>
2023-11-02 13:08:16 +0200

    fix: fix drop menu

--- src/main.js
+++ src/main.js
@@ -1,24 +1,5 @@
-const mobileMenu = document.querySelector('.mobile__menu');
+import mobMenu from './JS/mobMenu';
 
-console.log(mobileMenu);
-
-const burgerButton = document.querySelector('.burger__button');
-
-console.log(burgerButton);
-
-const closeButton = document.querySelector('.mobile__button__close');
-console.log(closeButton);
-
-function openMenu() {
-  mobileMenu.classList.toggle('is-open');
-  document.body.style.overflow = 'hidden';
-}
-
-function closeMenu() {
-  mobileMenu.classList.remove('is-open');
-  document.body.style.overflow = '';
-}
-
-burgerButton.addEventListener('click', openMenu);
-
-closeButton.addEventListener('click', closeMenu);
+window.addEventListener('DOMContentLoaded', () => {
+  mobMenu();
+});
